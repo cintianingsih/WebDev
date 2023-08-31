de17550bf0dbc7e1164d4a9c79ff4ab65d099bb4
cintianingsih <cintia.ningsih07@gmail.com>
2023-09-01 00:25:52 +0700

    update tugas_1

--- mission2/tugas_1/app.js
+++ mission2/tugas_1/app.js
@@ -1,106 +1,90 @@
-// const availableItems = document.querySelectorAll('.products')
-// const btnProd = document.querySelectorAll('.btn-prod')
-
-// console.log(availableItems)
-
-
-// const cartItem=[]
-
-// btnProd.forEach((prod) => {
-//     btn.addEventListener('click',()=>{
-//         console.log(btn.parentNode)
-//     })
-    
-// });
-
-document.addEventListener('DOMContentLoaded', function () {
-    const jmlbrg = document.getElementById('kuantitas');
-    const add = document.getElementById('add');
-    const min = document.getElementById('min');
-
-    const tampilkanBtn = document.getElementById("tampilkan-btn");
-    const nilaiTertampil = document.getElementById("nilai-tertampil");
-
-    let jumlahBarang = 0;
-
-    function updateJumlahBarang() {
-        jmlbrg.textContent = jumlahBarang;
-    }
-
-    add.addEventListener('click', function () {
-        jumlahBarang++;
-        updateJumlahBarang();
-    });
-
-    min.addEventListener('click', function () {
-        if (jumlahBarang > 0) {
-            jumlahBarang--;
-            updateJumlahBarang();
-        }
-    });         
-    updateJumlahBarang();
-
-    tampilkanBtn.addEventListener("click", function() {
-        const nilai = jumlahBarang
-        console.log("Nilai yang dimasukkan:", nilai);
-        if (jumlahBarang > 0){
-            nilaiTertampil.textContent = "x " + nilai;
-        }
-    });
-
-    fetch('products.json')
-    .then(response => response.json())
-    .then(products => {
-      const productList = document.querySelector('.product-list');
-
-      // Loop melalui setiap produk dalam data JSON
-      products.forEach(product => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-          <h2>${product.nama}</h2>
-          <img src="${product.gambar}" alt="${product.nama}">
-          <p><strong>Harga:</strong> ${product.harga}</p>
-          <p><strong>Deskripsi:</strong> ${product.deskripsi}</p>
-        `;
-        productList.appendChild(listItem);
+document.addEventListener("DOMContentLoaded", function() {
+    const containers = document.querySelectorAll(".quantity-container");
+    const tampilkanButtons = document.querySelectorAll(".tampilkan-button");
+    const quantityElements = document.querySelectorAll(".quantity");
+    const totalBelanjaElements = document.querySelectorAll(".total-belaja");
+    const decrementButtons = document.querySelectorAll(".decrement-button");
+    const incrementButtons = document.querySelectorAll(".increment-button");
+    const nilaiTertampilElements = document.querySelectorAll(".nilai-tertampil");
+    const totalKeseluruhanElement = document.querySelector(".total-keseluruhan"); 
+    const subtotalElement = document.querySelector(".subtotal"); 
+    const pajakElement = document.querySelector(".pajak"); 
+  
+    let jsonData; 
+    let totalKeseluruhan = 0; 
+  
+    fetch("products.json")
+      .then(response => response.json())
+      .then(data => {
+        jsonData = data;
+      })
+      .catch(error => {
+        console.error("Error fetching JSON data:", error);
       });
-    })
-    .catch(error => {
-      console.error('Terjadi kesalahan:', error);
+      
+    tampilkanButtons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        updateTotalBelanja(index);
+        totalBelanjaElements[index].style.display = "block";
+        calculateTotalKeseluruhan(); 
+      });
     });
-
-    const incrementButtons = document.querySelectorAll('.increment-button');
-    const decrementButtons = document.querySelectorAll('.decrement-button');
-
-    const totalQuantityElement = document.querySelector('.total-quantity');
-
-    let totalQuantity = 0;
-
-
-    incrementButtons.forEach(button => {
-        button.addEventListener('click', () => {
-        const quantityElement = button.nextElementSibling;
-        let quantity = parseInt(quantityElement.textContent);
-        quantityElement.textContent = ++quantity;
-        totalQuantity++;
-        totalQuantityElement.textContent = totalQuantity;
-        });
+  
+    decrementButtons.forEach((button, index) => {
+      button.addEventListener("click", () => updateQuantity(index, -1));
     });
-
-    decrementButtons.forEach(button => {
-        button.addEventListener('click', () => {
-        const quantityElement = button.previousElementSibling;
-        let quantity = parseInt(quantityElement.textContent);
-        if (quantity > 0) {
-            quantityElement.textContent = --quantity;
-            totalQuantity--;
-            totalQuantityElement.textContent = totalQuantity;
-            }
-        });
+  
+    incrementButtons.forEach((button, index) => {
+      button.addEventListener("click", () => updateQuantity(index, 1));
     });
-
-});
-
-
-
-
+  
+    function updateQuantity(index, change) {
+      const currentQuantity = parseInt(quantityElements[index].textContent);
+      const newQuantity = currentQuantity + change;
+  
+      if (newQuantity >= 0) {
+        quantityElements[index].textContent = newQuantity;
+      }
+    }
+  
+    function updateTotalBelanja(index) {
+      let totalBelanja = 0;
+      const quantity = parseInt(quantityElements[index].textContent);
+      const price = jsonData.products[index].price;
+      totalBelanja = quantity * price;
+  
+      if (quantity > 0) {
+        const product = jsonData.products[index];
+        const productInfo = `
+          <img src="${product.photo}" alt="${product.name}" style="width: 100px; height: 100px;">
+          <h2>${product.name}</h2>
+          <p>Price: Rp. ${product.price}</p>
+        `;
+  
+        nilaiTertampilElements[index].innerHTML = productInfo;
+        totalBelanjaElements[index].textContent = `Total Belanja: Rp. ${totalBelanja.toFixed(2)}`;
+        totalBelanjaElements[index].style.display = "block"; 
+      } else {
+        totalBelanjaElements[index].style.display = "none"; 
+      }
+    }
+  
+    function calculateTotalKeseluruhan() {
+      totalKeseluruhan = 0;
+      totalBelanjaElements.forEach(element => {
+        if (element.style.display !== "none") {
+          const totalBelanja = parseFloat(element.textContent.replace("Total Belanja: Rp. ", " "));
+          totalKeseluruhan += totalBelanja;
+        }
+      });
+  
+      const pajak = totalKeseluruhan * 0.11; 
+      const subtotal = totalKeseluruhan;
+      const totalKeseluruhanDenganPajak = totalKeseluruhan + pajak;
+  
+      subtotalElement.textContent = `Total Pembelian: Rp. ${subtotal.toFixed(2)}`;
+      pajakElement.textContent = `Pajak 11%: Rp. ${pajak.toFixed(2)}`;
+      totalKeseluruhanElement.textContent = `Total Bayar: Rp. ${totalKeseluruhanDenganPajak.toFixed(2)}`;
+    }
+  });
+  
