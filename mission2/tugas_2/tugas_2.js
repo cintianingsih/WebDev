1dcac7eb80564c973d0f754e7f4d393ab580edb1
cintianingsih <cintia.ningsih07@gmail.com>
2023-09-04 17:56:00 +0700

    update mission 2

--- mission2/tugas_2/tugas_2.js
+++ mission2/tugas_2/tugas_2.js
@@ -1,5 +1,5 @@
-const dino = document.getElementById("dino");
-const kaktus = document.getElementById("cactus");
+const gajah = document.getElementById("gajah");
+const pohon = document.getElementById("pohon");
 const playerScore = document.getElementById("score");
 
 let score = 0;
@@ -11,25 +11,25 @@ let jumlahScore = () => {
 };
 
 function jump() {
-    if (dino.classList != "animate") {
-        dino.classList.add("animate");
+    if (gajah.classList != "animate") {
+        gajah.classList.add("animate");
     }
     setTimeout(function () {
-        dino.classList.remove("animate");
+        gajah.classList.remove("animate");
     }, 500);
     let score = 0;
     interval = setInterval(jumlahScore, 100);
 }
 
-const ifHitCactus = setInterval(function () {
-    const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    const cactusleft = parseInt(window.getComputedStyle(kaktus).getPropertyValue("left"));
+const ifHitpohon = setInterval(function () {
+    const gajahTop = parseInt(window.getComputedStyle(gajah).getPropertyValue("top"));
+    const pohonleft = parseInt(window.getComputedStyle(pohon).getPropertyValue("left"));
 
-    if (cactusleft < 90 && cactusleft > 0 && dinoTop >= 80) {
-        kaktus.style.animation = "none";
-        kaktus.style.display = "none";
+    if (pohonleft < 90 && pohonleft > 0 && gajahTop >= 70) {
+        pohon.style.animation = "none";
+        pohon.style.display = "none";
 
-        const message = `Ups! Dino menabrak kaktus. Skor Anda : ${score}`;
+        const message = `Ups! lylia menabrak menabrak pohon. Skor Anda : ${score}`;
         alert(message);
 
         score = 0;
