cfd31e32695a88b9dcf0e9db0445e3b02acbdb14
cintianingsih <cintia.ningsih07@gmail.com>
2023-09-12 17:53:11 +0700

    add tugas Framework Exploration

---
+++ Framework Exploration/vue, express, mysql/backend/routes/routes.js
@@ -0,0 +1,30 @@
+//import functions from controller
+import express from "express";
+import {
+    showProducts,
+    showProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+} from "../controllers/product.js";
+
+//
+const router = express.Router();
+
+//
+router.get("/products", showProducts);
+
+//
+router.get("/products/:id", showProductById);
+
+//
+router.post('/products', createProduct);
+
+//
+router.put('/products/:id', updateProduct);
+
+//
+router.delete('/products/:id', deleteProduct);
+
+//
+export default router;
